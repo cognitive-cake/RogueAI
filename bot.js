16622ded403d787bf24ec94d0a8a8c2742b06627
cognitive-cake <nd.chernikov@gmail.com>
2018-09-12 15:45:57 +0300

    add cool embed posting

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-const Discord = require('discord.js');
+const { Client, RichEmbed } = require('discord.js');
 const auth = require('./auth.json');
-const client = new Discord.Client();
+const client = new Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -15,6 +15,22 @@ client.on('message', msg => {
         // Send the user's avatar URL
         msg.reply(msg.author.avatarURL);
     };
+
+    // If the message is "how to embed"
+    if (msg.content === 'how to embed') {
+        // We can create embeds using the MessageEmbed constructor
+        // Read more about all that you can do with the constructor
+        // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+        const embed = new RichEmbed()
+            // Set the title of the field
+            .setTitle('A slick little embed')
+            // Set the color of the embed
+            .setColor(0xFF0000)
+            // Set the main content of the embed
+            .setDescription('Hello, this is a slick embed!');
+        // Send the embed to the same channel as the message
+        msg.channel.send(embed);
+    }
 });
 
 client.login(auth.token);
